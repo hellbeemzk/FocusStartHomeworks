698b06ce24e51cbaa769aae1a2d25b5033e7088a
Konstantin Matvienko <mkg_95@mail.ru>
2021-11-21 08:32:36 +0700

    Создал проект, удалил сториборд

---
+++ CollectionApp/CollectionApp/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  CollectionApp
+//
+//  Created by Konstantin on 21.11.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ CollectionApp/CollectionApp/BooksViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BooksViewController.swift
+//  CollectionApp
+//
+//  Created by Konstantin on 21.11.2021.
+//
+
+import UIKit
+
+class BooksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
