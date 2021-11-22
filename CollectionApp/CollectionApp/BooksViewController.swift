465190c7d6791b2dcccf6aa4b1cc013bf1e8a2d5
Konstantin Matvienko <mkg_95@mail.ru>
2021-11-22 20:30:23 +0700

    Создал структуру книги,вынес констрейнты в enum,реализовал viewCollection

--- CollectionApp/CollectionApp/BooksViewController.swift
+++ CollectionApp/CollectionApp/BooksViewController.swift
@@ -7,13 +7,69 @@
 
 import UIKit
 
-class BooksViewController: UIViewController {
-
+final class BooksViewController: UIViewController {
+    
+    private lazy var collectionView: UICollectionView = {
+        let view = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        view.backgroundColor = .white
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupCollectionView()
+        title = "Рейтинг книг"
+    }
+}
+
+private extension BooksViewController {
+    
+    func setupCollectionView() {
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(collectionView)
+        collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    
+    func createLayout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: Constants.contentInsetsBooksVC, leading: Constants.contentInsetsBooksVC, bottom: Constants.contentInsetsBooksVC, trailing: Constants.contentInsetsBooksVC)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .absolute(Constants.heightDimensionGroupSize))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: Constants.contentInsetsForTopSecctionBooksVC, leading: 0, bottom: 0, trailing: 0)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+}
+
+extension BooksViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Book.arrayBooks.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+        cell.book = Book.arrayBooks[indexPath.item]
+        return cell
         
     }
-
-
 }
+
+extension BooksViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let detailVC = DetailViewController()
+        detailVC.book = Book.arrayBooks[indexPath.item]
+        self.navigationController?.pushViewController(detailVC, animated: true)
+    }
+}
+
+
+
 
---
+++ CollectionApp/CollectionApp/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  CollectionApp
+//
+//  Created by Konstantin on 21.11.2021.
+//
+
+import UIKit
+
+enum Constants {
+    static let heightDimensionGroupSize: CGFloat = 100
+    static let contentInsetsBooksVC: CGFloat = 5
+    static let contentInsetsForTopSecctionBooksVC: CGFloat = 10
+    static let indentForImageCenterYDetailVC: CGFloat = -80
+    static let heightForImageDetailVC: CGFloat = 150
+    static let widthForImageDetailVC: CGFloat = 100
+    static let indentBetweenLabelsDetailVC: CGFloat = 20
+    static let multiplierForLabelWidth: CGFloat = 0.8
+    static let multiplierForImageWidthCell: CGFloat = 1/6
+    static let indentForNameCell: CGFloat = 5
+    static let indentForButtonBottomModalVC: CGFloat = -30
+}
